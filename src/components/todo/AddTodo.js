c4a97418425019469c33bca25532eeeafecba5a6
Hidayt Rahman <hidaytrahman@gmail.com>
2023-01-23 19:09:33 +0530

    refactor(todo): with unit testing

--- src/components/todo/AddTodo.js
+++ src/components/todo/AddTodo.js
@@ -2,47 +2,63 @@ import { Button, TextField } from "@material-ui/core";
 import { useRef } from "react";
 import { useForm } from "react-hook-form";
 
-const AddTodo = ({addTodoItem}) => {
-    const refTodo = useRef(null);
+const AddTodo = ({ addTodoItem }) => {
+  const refTodo = useRef(null);
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => {
-        addTodoItem(data.todoTitle);
-        refTodo.current.value = "";
-        refTodo.current.focus();
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    addTodoItem(data.todoTitle);
+    refTodo.current.value = "";
+    refTodo.current.focus();
+  };
 
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="row">
+        <div className="col-sm-8 mb-2">
+          <TextField
+            error={
+              errors.todoTitle?.type === "required" ||
+              errors.todoTitle?.type === "maxLength"
+            }
+            label="Whats needs to be done? 🤔"
+            helperText={
+              (errors.todoTitle?.type === "required" && "Its required 😟") ||
+              (errors.todoTitle?.type === "maxLength" &&
+                "You've exceeds the max limits. Should not be more then 50 letter 🙄")
+            }
+            variant="filled"
+            fullWidth
+            inputRef={refTodo}
+            data-testid="todo-text"
+            {...register("todoTitle", {
+              required: {
+                value: true,
+                message: "Please write what is in your mind? 🤔",
+              },
+              maxLength: 50,
+            })}
+          />
+        </div>
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="row">
-                <div className="col-sm-8 mb-2">
-                    <TextField
-                        error={errors.todoTitle?.type === 'required' || errors.todoTitle?.type === 'maxLength'}
-                        label="Whats needs to be done? 🤔"
-                        helperText={
-                            errors.todoTitle?.type === 'required' && "Its required 😟" ||
-                            errors.todoTitle?.type === 'maxLength' && "You've exceeds the max limits. Should not be more then 50 letter 🙄"
-                        }
-                        variant="filled"
-                        fullWidth
-                        inputRef={refTodo}
-                        {...register("todoTitle", {
-                            required: {
-                                value: true,
-                                message: "Please write what is in your mind? 🤔"
-                            }, maxLength: 50
-                        })}
-                    />
-                </div>
-
-                <div className="col-sm-4 mb-2">
-                    <Button type="submit" variant="contained" color="primary" fullWidth
-                    >Add</Button>
-                </div>
-            </div>
-        </form>
-    )
-}
+        <div className="col-sm-4 mb-2">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            data-testid="todo-addBtn"
+          >
+            Add
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default AddTodo;
