cc2a1baee8f1e1f7758d101522ffd3e7b5640e7a
Hidayt Rahman <hidaytrahman@gmail.com>
2024-08-23 23:54:54 +0530

    refactor: firebase login code

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,53 +1,25 @@
-// import { FormControlLabel, FormGroup, Switch } from "@material-ui/core";
-import { FormControlLabel, FormGroup, Switch } from "@mui/material";
-import { useEffect, useState } from "react";
-import LoginWithGoogle from "./LoginWithGoogle";
+import NavTop from "./NavTop";
 
 const Header = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const currentTheme = localStorage.getItem("theme");
-
-    if (currentTheme === "dark") {
-      document.body.classList.add("dark-theme");
-      setDarkMode(true);
-    } else {
-      setDarkMode(false);
-      document.body.classList.remove("dark-theme");
-    }
-  }, [darkMode]);
-
-  const toggleChecked = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle("dark-theme");
-    const theme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
-    localStorage.setItem("theme", theme);
-  };
 
   return (
-    <header className="main-header">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-8">
-            <h1>🖊️Less - To Do </h1>
-            <h3>PenLess To Do gives you focus, from work to play. 😎</h3>
-          </div>
-          <div className="col-sm-4">
-            <LoginWithGoogle />
+    <>
+      <NavTop />
 
-            <FormGroup className="d-flex align-items-end">
-              <FormControlLabel
-                control={<Switch checked={darkMode} onChange={toggleChecked} />}
-                label="Dark Mode"
-              />
-            </FormGroup>
+      <header className="main-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-10">
+              <h1>🖊️Less - To Do </h1>
+              <h3>PenLess To Do gives you focus, from work to play. 😎</h3>
+            </div>
+            <div className="col-sm-2 d-flex">
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
+
   );
 };
 
